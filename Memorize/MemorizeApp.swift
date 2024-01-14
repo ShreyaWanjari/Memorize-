ed4f517aa7147213781758d16ce7aa6269d4cca8
ShreyaWanjari <shreyawanjari2001@gmail.com>
2024-01-14 23:08:25 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Shreya Wanjari on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
